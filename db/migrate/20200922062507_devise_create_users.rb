0dae78fc518063841c23848cb1699da389d48745
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-22 17:09:30 +0900

    Userモデルのカラム名編集

--- db/migrate/20200922062507_devise_create_users.rb
+++ db/migrate/20200922062507_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :name,               null: false
-      t.string :campany_name
+      t.string :company_name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_062507) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "campany_name"
+    t.string "company_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
