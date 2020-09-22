f76e27d45e681dcb04a3a0167795e265588f17a4
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-22 19:27:06 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "invalid."}
+  end
 end
