fe36266da925f3ff763dcadd06cfcabdab136c3c
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-28 00:47:18 +0900

    ItemモデルにCategory追加

--- db/migrate/20200922123349_create_items.rb
+++ db/migrate/20200922123349_create_items.rb
@@ -3,8 +3,9 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.references :user,               foreign_key: true
       t.string     :name,               null: false
-      t.integer    :status_id,          null: false
       t.text       :explanation,        null: false
+      t.integer    :category_id,        null: false
+      t.integer    :status_id,          null: false
       t.string     :size,               null: false
       t.integer    :delivery_fee_id,    null: false
       t.integer    :delivery_date_id,   null: false
