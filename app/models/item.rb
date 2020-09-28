2ca9431118b3767e62a09bd1b967f2ff8c7f2968
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-28 12:39:25 +0900

    Itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,21 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :delivery_date
+  belongs_to_active_hash :category
   has_one_attached :image
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :explanation
+    validates :size
+    validates :category_id,numericality:{ other_than: 0, message:"select!"}
+    validates :status_id,numericality:{ other_than: 0, message:"select!"}
+    validates :delivery_fee_id, numericality:{ other_than: 0, message:"select!"}
+    validates :delivery_date_id, numericality:{ other_than: 0, message:"select!"}
+    validates :price, format:{with: /\A[0-9]+\z/, message: "invalid. Price is half-width number" },
+                      numericality:{ only_integer:true }
+    
+  end
 end
