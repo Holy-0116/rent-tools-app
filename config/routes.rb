6cd21f1d16930dfd4344e5b31b38e33376e58b04
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-30 23:56:05 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root 'items#index'
   resources :items
+  resources :users, only: [:edit, :show, :update] do
+    resources :addresses, only: [:new]
+  end
 end
