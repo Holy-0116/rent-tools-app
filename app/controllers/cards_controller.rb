92eb6f53942c866e1cba487be1cdc0d54e9255d8
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-05 16:57:49 +0900

    Cardモデルのアクション作成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,35 @@
 class CardsController < ApplicationController
+  before_action :card_params, only: [:create]
+  
   def new
   end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    if params[:card_token] == nil
+      render :new
+      return
+    end
+    customer = Payjp::Customer.create(
+      description: "test",
+      card: params[:card_token])
+
+    card = Card.new(
+      user_id: current_user.id,
+      card_token: params[:card_token],
+      customer_token: customer.id)
+      binding.pry
+    if card.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+    private
+
+    def card_params
+      params.permit(:card_token, :user_id)
+    end
+  
 end
