19e5b39c490f8f036f7a04cb8eec1ff7fa74a9a6
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-05 16:05:48 +0900

    カード登録用のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   devise_for :users
   root 'items#index'
   resources :items
   resources :users, only: [:edit, :show, :update] do
     resources :addresses, only: [:new, :create, :edit, :update]
+    resources :cards, only: [:new, :create]
   end
 end
