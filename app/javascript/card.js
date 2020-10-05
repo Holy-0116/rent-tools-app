ec6f8d02a906b6ce30875c9408b9ff374db33513
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-05 16:57:31 +0900

    トークン作成のcard.js作成

---
+++ app/javascript/card.js
@@ -0,0 +1,34 @@
+const card = () =>{
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const form = document.getElementById("customer_form");
+  form.addEventListener("submit",function(e){
+    e.preventDefault();
+    const formResult = document.getElementById("customer_form");
+    const formData = new FormData(formResult);
+
+    const Card = {
+      number: formData.get("number"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+      cvc: formData.get("cvc")
+    };
+    Payjp.createToken(Card,function(status, response){
+      if (status === 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("customer_form");
+        const tokenObj = `<input value= ${token} type="hidden" name= "card_token">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+
+      document.getElementById("customer_form").submit();
+      document.getElementById("customer_form").reset();
+    })
+  });
+};
+// クレジットカード入力ページなら実行
+if (document.URL.match("/cards/new")){
+window.addEventListener('load', card)};
