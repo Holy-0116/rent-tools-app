cc247174de3dd4d3b4008472a37fb59253ae6449
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-20 15:16:39 +0900

    Orderコントローラーのカード選択アクション作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,41 @@
 class OrdersController < ApplicationController
-  # before_action :order_params, only: [:create]
   before_action :set_user
   before_action :set_item
+
+  def select_card
+    if current_user.card.present? 
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      cards = Card.where(user_id: current_user.id)
+      @cards = []
+      cards.each do |card|
+        @customer = Payjp::Customer.retrieve(card.customer_token)
+        @cards << @customer.cards
+      end
+    else
+      return
+    end
+  end
+
+  def set_default_card
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    cards = Card.where(user_id: current_user.id)
+    unless cards.present?
+      render :select_card
+      return
+    end
+    @cards = []
+    cards.each do |card|
+      @customer = Payjp::Customer.retrieve(card.customer_token)
+      @cards << @customer.cards
+    end
+    
+    index = params[:selected].to_i
+    @customer[:default_card] = @cards[0].data[index][:id]
+    @customer.save
+    redirect_to new_item_order_path
+    
+    
+  end
 
   def new
     @order = Order.new
@@ -49,6 +83,6 @@ class OrdersController < ApplicationController
       Payjp::Charge.create(
         :amount => params[:order][:price],
         :customer => customer.id,
-        :currency => 'jpy') 
+        :currency => 'jpy')         
   end
 end
