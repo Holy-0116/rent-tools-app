3340c2a9d1fd4d075103a85093ed895686063414
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-20 15:18:07 +0900

    cardsコントローラーのcard作成方法変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,24 +10,36 @@ class CardsController < ApplicationController
       cards = Card.where(user_id: current_user.id)
       @cards = []
       cards.each do |card|
-        customer = Payjp::Customer.retrieve(card.customer_token)
-        @cards << customer.cards
+        @customer = Payjp::Customer.retrieve(card.customer_token)
+        @cards << @customer.cards
       end
     else
       return
     end
   end
+  
 
   def create
+    # card_tokenが正しいか確認
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     if params[:card_token] == nil
       redirect_to new_user_card_path
       return
     end
+    # ユーザーのcustomer_tokenが存在する場合
+    if Card.find_by(user_id: current_user.id)
+      card = current_user.card
+      customer = Payjp::Customer.retrieve(card[0][:customer_token])
+      customer.cards.create(
+        card: params[:card_token]
+      )
+    else
+    # ユーザーがcustomer_tokenが存在していない場合 
     customer = Payjp::Customer.create(
       description: "test",
-      card: params[:card_token])
-
+      card: params[:card_token],)
+    end
+    # cardテーブルに保存
     card = Card.new(
       user_id: current_user.id,
       card_token: params[:card_token],
