1f97dc590511baace7a6475f373527c139879413
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-28 16:49:55 +0900

    FactoryBot(:item)編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,5 +10,9 @@ FactoryBot.define do
     delivery_date_id {2}
     stock            {1}
     price            {"1000"}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
