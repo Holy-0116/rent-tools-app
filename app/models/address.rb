5ae7e9dedaa7f272dfb186bf36fa065d9fc08b10
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-29 14:17:12 +0900

    モデルのmessage編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,10 +4,10 @@ class Address < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-  validates :postal_code   ,format: {with:/\A\d{7}\z/, message: "is only number. input half-width number"}
+  validates :postal_code   ,format: {with:/\A\d{7}\z/, message: " is only number. input half-width number"}
   validates :prefecture_id ,numericality: {greater_than: 0, message: "select"}
   validates :city_name
   validates :house_number
-  validates :phone_number  ,format: {with:/\A\d{10,11}\z/, message: "is only number. input half-width number"}
+  validates :phone_number  ,format: {with:/\A\d{10,11}\z/, message: " is only number. input half-width number"}
   end
 end
