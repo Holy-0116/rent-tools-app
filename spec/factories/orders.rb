9cc815033e47194040d46bbd404a1f1f1efd6d0c
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-29 10:38:42 +0900

    FactoryBot(:order)作成

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order do
-    
+    association :borrower_id, factory: :user
+    association :item
+    piece       {100}
+    start_date  {'2030-1-1'}
+    period      {1}
+    price       {1}
   end
 end
