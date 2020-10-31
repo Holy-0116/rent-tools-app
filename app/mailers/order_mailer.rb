2cfbd0bdd725255f2806be7e25a65c7067fcf903
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-10-31 11:00:27 +0900

    メールデバッグ処理

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,9 @@
 class OrderMailer < ApplicationMailer
   def send_when_order_create(order)
-    @lender = order.lender
-    @borrower = order.borrower 
+    @order = order
+    @item = @order.item
+    @lender = @item.user
+    @borrower = @order.borrower 
     @url = "https://rent-tools-app.herokuapp.com/"
     mail  to: @lender.email,
           subject: 'レンタルの注文がありました'
