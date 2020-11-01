3898999ca3864f56d0556b148b157ec24612f0b6
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-01 19:34:50 +0900

    Userモデルのエラーメッセージ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     validates :email
     
   end
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: " invalid."}, on: :create
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, on: :create
 
 
   has_many :items
