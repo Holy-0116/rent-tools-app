054c69bee2d0acbfe1454e2b2263f6ab9056c77c
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-03 20:35:17 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'cards/new'
   devise_for :users
   root 'items#index'
   resources :items do
-    collection do
-      get :search
-    end
     resource :order do
       collection do
         get :select_card
@@ -17,9 +13,10 @@ Rails.application.routes.draw do
         get :new_card
         post :create_card
       end
-      
     end
   end
+  get 'search/keyword'
+  get 'search/sort_order'
   resources :users, only: [:edit, :show, :update] do
     resource :address, only: [:new, :create, :edit, :update]
     resource :card, only: [:new, :create, :show]
