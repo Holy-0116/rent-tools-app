9478772df83139503b1ac59c4a0ecd6f227c8047
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-03 20:42:32 +0900

    Applicationコントローラーに@p定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
-class ApplicationController < ActionController::Base
-  
+class ApplicationController < ActionController::Base  
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :keyword
 
   private
 
@@ -8,4 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :company_name])
   end
 
+  def keyword
+    @p = Item.ransack(params[:q])
+  end
 end
