cf1805893063db7ff9c6b6a7bda6111b7c703019
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-24 00:20:51 +0900

    モデルのアソシエーション

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ActiveHash::Base
   self.data = [
-    {id: 0, name: "パレットナイフ"},
-    {id: 1, name: "セルクル"},
-    {id: 2, name: "口金"},
-    {id: 3, name: "回転台"},
-    {id: 4, name: "抜き型"},
-    {id: 5, name: "焼き型"},    
+    {id: 1, name: "パレットナイフ"},
+    {id: 2, name: "セルクル"},
+    {id: 3, name: "口金"},
+    {id: 4, name: "回転台"},
+    {id: 5, name: "抜き型"},
+    {id: 6, name: "焼き型"},    
   ]
 end
