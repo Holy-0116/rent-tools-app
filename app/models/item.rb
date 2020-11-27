b43fca7d65418353cdc28b44aca60b0ebc8eeeb9
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-28 08:53:29 +0900

    Itemモデル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   with_options presence: true do
     validates :name
@@ -23,4 +24,27 @@ class Item < ApplicationRecord
     
   end
 
+  def create_notification_comment(current_user, comment_id)
+    # 商品にコメントしたユーザーへの通知作成（本人と出品者を除く）
+    temp_ids = Comment.select(:borrower_id).where(item_id: id).where.not("borrower_id=? or borrower_id=?", current_user.id,user_id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment(current_user, comment_id, temp_id[:borrower_id])
+    end
+    # 出品者への通知作成（重複しないように別で作成）
+      save_notification_comment(current_user, comment_id, user_id) 
+   
+  end
+
+  def save_notification_comment(current_user, comment_id, visited_id)
+    # コメント投稿者が出品者であった時は通知は作成しない
+    if current_user.id != visited_id
+      notification = current_user.active_notifications.new(
+        item_id: id,
+        comment_id: comment_id,
+        visited_id: visited_id,
+        action: 'comment'
+      )
+      notification.save if notification.valid?
+    end
+  end
 end
