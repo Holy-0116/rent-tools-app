6d0138dc41c3eaf9a87b510134a1e0f54e0db306
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-29 00:24:49 +0900

    destroyアクションの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :item
   belongs_to :borrower, class_name: "User", foreign_key: "borrower_id"
-  has_many :nofitications, dependent: :destroy
-
+  has_many   :notifications, dependent: :destroy
+            
   validates :text, length:{maximum: 120}, presence: true
 
   
