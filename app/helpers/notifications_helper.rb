f76f940b3f15bfc2b021930aa6e681589fc92dfe
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-28 17:13:49 +0900

    通知確認メソッド定義

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,12 @@
 module NotificationsHelper
+  # 通知を確認したらcheckedをtrueにするヘルパーメソッド
+  def notification_checkded
+    @notifications.each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+
+  def unchecked_notifications
+    current_user.passive_notifications.where(checked: false)
+  end
 end
