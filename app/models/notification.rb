2500b0e285828c59a3b0d27314c86b93c09231ac
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-11-30 17:31:35 +0900

    Notificationモデルの修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,16 @@ class Notification < ApplicationRecord
   belongs_to :comment, optional: true
   belongs_to :order, optional: true
   belongs_to :item
-  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
-  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id'
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
 
+  validates :action ,presence: true
+  validates :checked, inclusion: {in: [true, false]}
+  validates :comment_or_order, presence: true
+
+  private
+  
+    def comment_or_order
+      comment.presence or order.presence
+    end
 end
--- db/migrate/20201124071922_create_notifications.rb
+++ db/migrate/20201124071922_create_notifications.rb
@@ -1,13 +1,13 @@
 class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
-      t.integer :visitor_id, null: false
-      t.integer :visited_id, null: false
-      t.references :item, foreign_key: true
-      t.references :comment, foreign_key: true
-      t.references :order, foreign_key: true
-      t.string  :action 
-      t.boolean :checked, default: false, null: false
+      t.references :visitor,    foreign_key: { to_table: :users }, null: false
+      t.references :visited,    foreign_key: { to_table: :users }, null: false
+      t.references :item,       foreign_key: true
+      t.references :comment,    foreign_key: true
+      t.references :order,      foreign_key: true
+      t.string     :action,     null: false
+      t.boolean    :checked,    default: false, null: false
       t.timestamps
     end
   end
