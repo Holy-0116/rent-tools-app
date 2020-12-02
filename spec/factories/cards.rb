a3cc29529136e17123922e598a1b35082ba718f3
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-12-02 11:52:45 +0900

    クレジットカード登録機能モデルテスト

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    association :user
+    card_token        {"tok_f60baca131e8587e692798c7809b"}
+    customer_token    {"cus_22c9c6fd89310c79976b592edf30"}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @card = FactoryBot.build(:card)
+  end
+
+  describe "クレジットカード情報登録機能" do
+    context "クレジットカード情報が登録できるとき" do
+      it "全ての値があれば登録できる" do
+        expect(@card).to be_valid
+      end
+    end
+
+    context "クレジットカード情報が登録できないとき" do
+      it "ユーザーがいないと登録できない" do
+        @card.user = nil
+        @card.valid?
+        expect(@card.errors.full_messages).to include("Userを入力してください")
+      end
+      it "card_tokenがないと登録できない" do
+        @card.card_token= nil
+        @card.valid?
+        expect(@card.errors.full_messages).to include("Card tokenを入力してください")
+      end
+      it "customer_tokenがないと登録できない" do
+        @card.customer_token = nil
+        @card.valid?
+        expect(@card.errors.full_messages).to include("Customer tokenを入力してください")
+      end
+    end
+  end
 end
